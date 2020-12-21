505557112045b078019fcfe91250e820614aa753
NkaniY <yuuki.072395@gmail.com>
2020-12-22 00:59:34 +0900

    Initial commit

---
+++ app/src/main/java/jp/co/nkaniy/overlaycomment/MainActivity.kt
@@ -0,0 +1,13 @@
+package jp.co.nkaniy.overlaycomment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
